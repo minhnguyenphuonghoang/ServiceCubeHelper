49b043fd006d3520a763d5c2ecbd76171fb1cf46
Minh Nguyen <minhnguyenphuonghoang@gmail.com>
2017-02-08 02:03:37 +0700

    night

--- app/src/main/java/com/example/administrator/servicecubehelper/MainActivity.java
+++ app/src/main/java/com/example/administrator/servicecubehelper/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.administrator.servicecubehelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.usercreator.UserCreator;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        switchOnUserCreatorActivity();
     }
+
+
+
+
+
+
+    public void switchOnUserCreatorActivity(View v){
+        Toast.makeText(MainActivity.this, "Switched to USER CREATOR activity", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, UserCreator.class));
+    }
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/usercreator/UserCreator.java
@@ -0,0 +1,15 @@
+package com.usercreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.servicecubehelper.R;
+
+public class UserCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_creator);
+    }
+}
